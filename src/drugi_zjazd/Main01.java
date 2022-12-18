388d6bd776ee63fae4e9796c6461d08f04bda550
pierunek <pierunmaster@gmail.com>
2022-12-18 15:28:08 +0100

    Zadania z ćwiczeń - drugi zjazd

--- src/drugi_zjazd/Main01.java
+++ src/drugi_zjazd/Main01.java
@@ -13,8 +13,9 @@ public class Main01 {
             result += counter;
             counter++;
         }
-        System.out.println(counter);
+        System.out.println("ilość kolejnych liczb do zsumowaia: " + counter);
 
+        separator();
 
         //to co wyżej ale dla parzystych i nieparzystych
         int sumaParzyste = 0;
@@ -25,18 +26,19 @@ public class Main01 {
 
         while (sumaParzyste < 1000 && sumaNieparzyste < 1000) {
             if (globalCounter % 2 ==0) {
-                System.out.printf("sumaParzyste %d add %d result %d\n", sumaParzyste, globalCounter, sumaParzyste + globalCounter);
+//                System.out.printf("sumaParzyste %d add %d result %d\n", sumaParzyste, globalCounter, sumaParzyste + globalCounter);
                 sumaParzyste+=globalCounter;
                 parzysteCounter++;
             } else {
-                System.out.printf("sumaNieparzyste %d add %d result %d\n", sumaNieparzyste, globalCounter, sumaNieparzyste + globalCounter);
+//                System.out.printf("sumaNieparzyste %d add %d result %d\n", sumaNieparzyste, globalCounter, sumaNieparzyste + globalCounter);
                 sumaNieparzyste+=globalCounter;
                 nieparzysteCounter++;
             }
             globalCounter++;
         }
-        System.out.printf("Nieparzyste %d, Parzyste %d", nieparzysteCounter, parzysteCounter);
+        System.out.printf("Nieparzyste %d, Parzyste %d\n", nieparzysteCounter, parzysteCounter);
 
+        separator();
 
         //100 kolejnych liczb po 4 w wierszu
         for (int i = 0; i < 100; i++) {
@@ -46,32 +48,42 @@ public class Main01 {
                 System.out.print(i + "\t");
         }
 
+        separator();
+
         //Scanner imie i nazwisko
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj imie i nazwisko: ");
         String s = scanner.nextLine();
-        System.out.print("Hello\n" + s);
+        System.out.print("Hello " + s);
+
+        separator();
 
         //Scanner sumowanie integerów
         System.out.println("Podaj liczbę:");
         int i = scanner.nextInt();
-        System.out.println("Podaj liczbę:");
+        System.out.println("Podaj drugą liczbę:");
         int j = scanner.nextInt();
         System.out.printf("%d + %d = %d", i, j, i+j);
 
+        separator();
+
         //Scanner pole czegoś tam przy użyciu double
-        System.out.println("Podaj liczbę:");
+        System.out.println("Podaj radius (double):");
         double num = scanner.nextDouble();
         System.out.printf("Area: %.2f",2 * Math.PI * num);
 
+        separator();
+
         //Scanner sprawdzający czy wpisana liczba jest parzysta czy nie
-        System.out.println("Podaj numer:");
+        System.out.println("Podaj liczbę:");
         int evenOrOddNum = scanner.nextInt();
         if (evenOrOddNum % 2 == 0) {
             System.out.println(1);
         } else {
             System.out.println(0);
         }
+
+        separator();
 
         //Scanner w pętli do wpisywania i wyświetlania 5 imion
         int n = 0;
@@ -85,6 +97,8 @@ public class Main01 {
             System.out.println("Hello " + name);
         }
 
+        separator();
+
         //Scanner wypisujący n-podanych elementów od 1.
         System.out.println("How many elements?");
         int[] numArray = new int[scanner.nextInt()];
@@ -97,6 +111,8 @@ public class Main01 {
             g++;
         }
 
+        separator();
+
         //Scanner przyjmujący 10 integerów od usera i wyświetlający je razem z indeksami tablicy
         int[] integers = new int[10];
         for (int h = 0; h < integers.length; h++) {
@@ -106,6 +122,11 @@ public class Main01 {
         for (int h = 0; h < integers.length; h++) {
             System.out.printf("tab[%d] = %d\n", h, integers[h]);
         }
+
+    }
+
+    private static void separator() {
+        System.out.println("\n===========================================");
     }
 
 }
